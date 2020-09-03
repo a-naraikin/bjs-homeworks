11a7c48d69db1eafd7ab5e9ede7fbebd380c4084
a-naraikin <anna.naraikin@yandex.ru>
2020-09-03 21:06:58 +0300

    Task 1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,25 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+'use strict'
+
+function getResult(a, b, c){
+		let discriminant = Math.pow(b, 2) - 4 * a * c;
+	
+		let array = [];
+	
+ 		if (discriminant < 0) {
+ 			array;
+ 		} else if (discriminant === 0) {
+ 			let rootOne = -b / 2 * a;
+ 			array.push(rootOne);
+ 		} else {
+			let rootOneOfTwo = (-b + Math.sqrt(discriminant)) / 2 * a;
+			array.push(rootOneOfTwo);
+ 			let rootTwoOfTwo = (-b - Math.sqrt(discriminant)) / 2 * a;
+ 			array.push(rootTwoOfTwo);
+ 		}
+	
+		let result = array;
+	
+    return result;
 }
 
 function getAverageMark(marks){
