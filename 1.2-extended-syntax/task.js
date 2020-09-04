d3fcf54815c40b6c6c240b079c725e3d009c451e
a-naraikin <66479710+a-naraikin@users.noreply.github.com>
2020-09-04 10:26:03 +0300

    Update task.js
    Shortened the code.

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,23 +2,21 @@
 
 function getResult(a, b, c){
   let discriminant = Math.pow(b, 2) - 4 * a * c;
-	
   let array = [];
-
-  if (discriminant < 0) {
-    array;
-  } else if (discriminant === 0) {
+	
+  if (discriminant === 0) {
     let rootOne = -b / 2 * a;
     array.push(rootOne);
-  } else {
+  } else if (discriminant > 0) {
     let rootOneOfTwo = (-b + Math.sqrt(discriminant)) / 2 * a;
     array.push(rootOneOfTwo);
     let rootTwoOfTwo = (-b - Math.sqrt(discriminant)) / 2 * a;
     array.push(rootTwoOfTwo);
   }
-    let result = array;
 	
-    return result;
+  let result = array;
+	
+  return result;
 }
 
 function getAverageMark(marks){
@@ -37,29 +35,19 @@ function getAverageMark(marks){
   if (marks.length > 0) {
     let sum = 0;
     for (let i = 0; i < arrayMark.length; i++) {
-      let element = arrayMark[i];
-      sum = sum + element;
+      sum += arrayMark[i];
     }
 
-    averageMark = sum / arrayMark.length;
+    return sum / arrayMark.length;
   }
-	
-  return averageMark;
 }
 
 function askDrink(name, dateOfBirthday){
-  let yearOfBirthday = dateOfBirthday.getFullYear();
-	
-  let year = new Date().getFullYear();
-  let age = year - yearOfBirthday;
-	
-  let result;
+  let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
 
   if (age >= 18) {
-    result = `Не желаете ли олд-фэшн, ${name}?`;
+    return `Не желаете ли олд-фэшн, ${name}?`;
   } else {
-    result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
   }
-	
-  return result;
 }
