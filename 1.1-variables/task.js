67d3c72b9fb7e1146f783525febded724b775b3b
a-naraikin <66479710+a-naraikin@users.noreply.github.com>
2020-09-04 09:54:54 +0300

    Update task.js
    Remove extra margins.

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -1,33 +1,33 @@
- /*
-	Кратко о работе с функциями:
-	1) Значения в скобках - это аргументы, которые мы получаем в момент вызова функции. Их и надо присваивать требуемым в заданиях переменным.
-	2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
-	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
- */
+/*
+  Кратко о работе с функциями:
+  1) Значения в скобках - это аргументы, которые мы получаем в момент вызова функции. Их и надо присваивать требуемым в заданиях переменным.
+  2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
+  3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
+*/
 
 function averageMark(a, g, p) {
-	let algebra = a,
-		geography = g,
-		physics = p;
+  let algebra = a,
+      geography = g,
+      physics = p;
 
-	let taskResult = (algebra + geography + physics) / 3;
+  let taskResult = (algebra + geography + physics) / 3;
 
-	return taskResult; 
+  return taskResult; 
 }
 
 function sayHello(userName) {
-	let myName = userName;
-	let message = `Привет, мир! Меня зовут ${myName}`
+  let myName = userName;
+  let message = `Привет, мир! Меня зовут ${myName}`
 
-	return message;
+  return message;
 }
 
 function calculateFormula() {
-    let x = 2
-      , y = 22
-      , z = 0;
+  let x = 2
+    , y = 22
+    , z = 0;
 
-    let result = x * y + 5 * z + x - 1;
+  let result = x * y + 5 * z + x - 1;
 
-    return result;
+  return result;
 }
