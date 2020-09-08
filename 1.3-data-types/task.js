6860348344c52652db9ece5009ed6861bd2f1d78
a-naraikin <66479710+a-naraikin@users.noreply.github.com>
2020-09-08 20:06:56 +0300

    Update task.js
    Removed the extra padding

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,53 +1,45 @@
 'use strict'
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // проверка на число
-	if (isNaN(percent))
-        console.log(`Параметр ${'Процентная ставка'} содержит неправильное значение ${percent}`);
-	if (isNaN(contribution))
-		console.log(`Параметр ${'Начальный взнос'} содержит неправильное значение ${contribution}`);
-	if (isNaN(amount))
-		console.log(`Параметр ${'Общая стоимость'} содержит неправильное значение ${amount}`);
+  if (isNaN(percent))
+    console.log(`Параметр ${'Процентная ставка'} содержит неправильное значение ${percent}`);
+  if (isNaN(contribution))
+    console.log(`Параметр ${'Начальный взнос'} содержит неправильное значение ${contribution}`);
+  if (isNaN(amount))
+    console.log(`Параметр ${'Общая стоимость'} содержит неправильное значение ${amount}`);
     
-    let bodyCredit = amount - contribution;
-    console.log(bodyCredit);
+  let bodyCredit = amount - contribution;
     
-    let dates = new Date(); // сегодняшняя дата
+  let dates = new Date(); // сегодняшняя дата
 
-    let months;
-        months = (date.getFullYear() - dates.getFullYear()) * 12;
-        months -= date.getMonth() + 1;
-        months += dates.getMonth();
-        months <= 0 ? 0 : months;
-        let totalMonths = months + 1; // кол-во месяцев
-    console.log(totalMonths);
+  let months;
+      months = (date.getFullYear() - dates.getFullYear()) * 12;
+      months -= date.getMonth() + 1;
+      months += dates.getMonth();
+      months <= 0 ? 0 : months;
+  let totalMonths = months + 1; // кол-во месяцев
 
-    let rate = percent / 100 / 12; // % 
+  let rate = percent / 100 / 12; // % 
+  let manthPlay = bodyCredit * (rate + rate / (((1 + rate)** totalMonths) - 1)); // ежемесячная оплата
+  let sum = manthPlay * totalMonths; // общая сумма
+  let totalAmount = sum.toFixed(2); // округление числа
 
-    let manthPlay = bodyCredit * (rate + rate / (((1 + rate)** totalMonths) - 1)); // ежемесячная оплата
-    console.log(manthPlay);
-
-    let sum = manthPlay * totalMonths; // общая сумма
-
-    let totalAmount = sum.toFixed(2); // округление числа
-
-    console.log(totalAmount);
-    return Number(totalAmount);
+  console.log(totalAmount);
+  return Number(totalAmount);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    let a = name;
+  let a = name;
 
-    let checkValue = Boolean(name);
-    let greeting;
+  let checkValue = Boolean(name);
+  let greeting;
 
-	if (checkValue == 1) {
-		greeting = `Привет, мир! Меня зовут ${name}`;
-	} else {
-		greeting = `Привет, мир! Меня зовут Аноним`;
-    }
+  if (checkValue == 1) {
+    greeting = `Привет, мир! Меня зовут ${name}`;
+  } else {
+    greeting = `Привет, мир! Меня зовут Аноним`;
+  }
 
-    console.log(greeting);
-    return greeting;
+  console.log(greeting);
+  return greeting;
 }
