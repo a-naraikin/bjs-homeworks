5928d82a0f30c4f8df8a23f926b70d6048c088d8
a-naraikin <66479710+a-naraikin@users.noreply.github.com>
2020-10-13 20:38:59 +0300

    Update task.js

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -3,63 +3,61 @@
 console.log('=========== 1 Task ===========\n\n')
 
 function getSolutions(a, b, c){
-	let D = Math.pow(b, 2) - 4 * a * c;
-	let x1, x2;
+  let D = Math.pow(b, 2) - 4 * a * c;
+  let x1, x2;
 	
-	const x = {D: D, roots: []};
+  const x = {D: D, roots: []};
 	
-	if (D == 0) {
-		x1 = -b / (2 * a);
-		x.roots.push(x1);
+  if (D == 0) {
+    x1 = -b / (2 * a);
+    x.roots.push(x1);
 		
-	} else if (D > 0) {
-		x1 = (-b + Math.sqrt(D)) / (2 * a);
-		x2 = (-b - Math.sqrt(D)) / (2 * a);
-		x.roots.push(x1, x2);
-  }
-	
-	return x;
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    x.roots.push(x1, x2);
+  }	
+  return x;
 };
 
 function showSolutionsMessage(a, b, c){
-	let result = getSolutions(a, b, c);
+  let result = getSolutions(a, b, c);
 	
-	console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
-	console.log(`Значение дискриминанта: ${result.D}`);
+  console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+  console.log(`Значение дискриминанта: ${result.D}`);
 	
-	if (result.roots == 0) {
-		console.log(`Уравнение не имеет вещественных корней`);
-	} else if (result.roots.length == 1) {
-		console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+  if (result.roots == 0) {
+    console.log(`Уравнение не имеет вещественных корней`);
+  } else if (result.roots.length == 1) {
+    console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
   } else if (result.roots.length == 2) {
-	console.log(`Уравнение имеет два корня X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
-	}
-	
-	return;
+    console.log(`Уравнение имеет два корня X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+  }	
+  return;
 }
 
 console.log('\n=========== 2 Task ===========\n\n')
 
 function getAverageScore(data){	
-	let subjects = {};
-	let sumAverage = 0;
-	let countIterations = 0;
+  let subjects = {};
+  let sumAverage = 0;
+  let countIterations = 0;
 	
-	for (let nameSubject in data) {
-		let averageScore = getAverageMark(data[nameSubject]);
+  for (let nameSubject in data) {
+    let averageScore = getAverageMark(data[nameSubject]);
 		
-		subjects[nameSubject] = averageScore;
-		sumAverage += averageScore;
-		countIterations++;
-	}
+    subjects[nameSubject] = averageScore;
+    sumAverage += averageScore;
+    countIterations++;
+  }
 	
-	if (!sumAverage){
-		subjects.average = 0;
-	} else {
-		subjects.average = sumAverage / countIterations;
-	}
-	
-	return subjects;
+  if (!sumAverage){
+    subjects.average = 0;
+  } else {
+    subjects.average = sumAverage / countIterations;
+  }
+
+  return subjects;
 }
 
 function getAverageMark(marks){
@@ -69,41 +67,40 @@ function getAverageMark(marks){
   if (len == 0) {
     return averageMark;
   } else {
-		let sum = 0;
+    let sum = 0;
     for (let i = 0; i < len; i++) {
       sum += marks[i];
     }
     return sum / len;
-	}
+  }
 }
 
 console.log('\n=========== 3 Task ===========\n\n')
 
 function getPersonData(secretData){
-	let subjects = {};
+  let subjects = {};
 	
-	for (let nameSubject in secretData) {
-		if (nameSubject == 'aaa'){
-			subjects['firstName'] = getDecodedValue(secretData[nameSubject]);
-			delete subjects[nameSubject];
-		}
-		if (nameSubject == 'bbb'){
-			subjects['lastName'] = getDecodedValue(secretData[nameSubject]);
-			delete subjects[nameSubject];
-		}
-	}
+  for (let nameSubject in secretData) {
+    if (nameSubject == 'aaa'){
+      subjects['firstName'] = getDecodedValue(secretData[nameSubject]);
+      delete subjects[nameSubject];
+    }
+    if (nameSubject == 'bbb'){
+      subjects['lastName'] = getDecodedValue(secretData[nameSubject]);
+      delete subjects[nameSubject];
+    }
+  }
 	
-	return subjects;
+  return subjects;
 }
 
 function getDecodedValue(secret){
-	if (secret == 0){
-		secret = "Родриго";
-	}
+  if (secret == 0){
+    secret = "Родриго";
+  }
 	
-	if (secret == 1){
-		secret = "Эмильо";
-	}
-	
-	return secret;
+  if (secret == 1){
+    secret = "Эмильо";
+  }
+  return secret;
 }
