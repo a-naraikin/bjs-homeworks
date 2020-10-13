2258767d3a8f0b34e60e602f786a6743cf28e0d9
a-naraikin <66479710+a-naraikin@users.noreply.github.com>
2020-10-13 19:58:55 +0300

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -2,35 +2,33 @@
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   if (isNaN(percent))
-    console.log(`Параметр ${'Процентная ставка'} содержит неправильное значение ${percent}`);
+    return `Параметр ${'Процентная ставка'} содержит неправильное значение ${percent}`;
   if (isNaN(contribution))
-    console.log(`Параметр ${'Начальный взнос'} содержит неправильное значение ${contribution}`);
+    return `Параметр ${'Начальный взнос'} содержит неправильное значение ${contribution}`;
   if (isNaN(amount))
-    console.log(`Параметр ${'Общая стоимость'} содержит неправильное значение ${amount}`);
+    return `Параметр ${'Общая стоимость'} содержит неправильное значение ${amount}`;
     
-  let bodyCredit = amount - contribution;
+  const bodyCredit = amount - contribution;
     
-  let dates = new Date(); // сегодняшняя дата
+  const dates = new Date(); // сегодняшняя дата
 
   let months;
       months = (date.getFullYear() - dates.getFullYear()) * 12;
       months -= date.getMonth() + 1;
       months += dates.getMonth();
       months <= 0 ? 0 : months;
-  let totalMonths = months + 1; // кол-во месяцев
+  const totalMonths = months + 1; // кол-во месяцев
 
-  let rate = percent / 100 / 12; // % 
-  let manthPlay = bodyCredit * (rate + rate / (((1 + rate)** totalMonths) - 1)); // ежемесячная оплата
-  let sum = manthPlay * totalMonths; // общая сумма
-  let totalAmount = sum.toFixed(2); // округление числа
+  const rate = percent / 100 / 12; // % 
+  const manthPlay = bodyCredit * (rate + rate / (((1 + rate)** totalMonths) - 1)); // ежемесячная оплата
+  const sum = manthPlay * totalMonths; // общая сумма
+  const totalAmount = sum.toFixed(2); // округление числа
 
   console.log(totalAmount);
   return Number(totalAmount);
 }
 
 function getGreeting(name) {
-  let a = name;
-
   let checkValue = Boolean(name);
   let greeting;
 
