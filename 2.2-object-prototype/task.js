b793d37cdd62ae54a4775c29d7225973ee58bb44
a-naraikin <anna.naraikin@yandex.ru>
2020-10-28 12:01:37 +0300

    Task 1

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,27 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict'
+
+console.log('\n=========== 1 Task ===========\n\n');
+
+String.prototype.isPalindrome = function() {
+	const str = this.toLowerCase();
+	
+	let arrayOriginal = str.split('');
+	let arrayReverse = str.split('').reverse();
+	
+	function deleteSpace(array){
+		for (let i = 0; i < array.length; i++){
+  			if (array[i] == " "){
+      			array.splice(i, 1);
+				}
+    	}
+		return array.join('');
+	};
+
+	let strOne = deleteSpace(arrayOriginal),
+			strTwo = deleteSpace(arrayReverse);
+
+	return strOne === strTwo;
+};
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
