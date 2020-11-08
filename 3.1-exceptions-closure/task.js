ac2d780480d49b3f038ab8ec63cf083369753e1c
a-naraikin <anna.naraikin@yandex.ru>
2020-11-08 18:49:19 +0300

    Task 2

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -19,3 +19,43 @@ function validateCount(value){
 		return err;
 	}
 }
+
+console.log('\n ----------- 2 task ----------- \n\n');
+
+class Triangle{
+	constructor(a, b, c){
+		const isExistTriangle = a + b > c && a + c > b && b + c > a;
+		if (!isExistTriangle) {
+			let err = new Error("Треугольник с такими сторонами не существует")
+			throw err;
+		}
+		
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	getPerimeter() {
+		const perimeter = this.a + this.b + this.c;
+		return perimeter;
+	}
+	
+	getArea() {
+		let p = this.getPerimeter() / 2; 
+  	const s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return Number(s.toFixed(3));
+	}
+}
+
+class ErrorTriangele {
+	getPerimeter() { return "Ошибка! Треугольник не существует"; }
+	getArea() { return "Ошибка! Треугольник не существует"; }
+}
+
+function getTriangle(a, b, c){
+	try{
+		return new Triangle(a, b, c);
+	} catch(err){
+		return new ErrorTriangele();
+	}
+}
