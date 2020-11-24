19e81aff1da5c283a3ffbc50e47078a7d2cafc6c
a-naraikin <66479710+a-naraikin@users.noreply.github.com>
2020-11-24 17:27:38 +0300

    update file

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -3,62 +3,62 @@ console.clear();
 //----------- 1 task -----------
 
 const weapons = [
-	new Knife(), 
-	new Staff(), 
-	new Axe(), 
-	new StormStaff(), 
-	new LongBow(), 
-	new Bow()
+  new Knife(), 
+  new Staff(), 
+  new Axe(), 
+  new StormStaff(), 
+  new LongBow(), 
+  new Bow()
 ];
 
 function getNames(){
-	return weapons.map(function(elem){
-		return elem.name;
-	});
+  return weapons.map(function(elem){
+    return elem.name;
+  });
 }
 
 function getCountReliableWeapons(element) {
-	let arrDurability = [];
+  let arrDurability = [];
 	
-	weapons.filter(function(elem){
-		if (element < elem.durability){
-			arrDurability.push(elem.durability);
-		}
-	});
+  weapons.filter(function(elem){
+    if (element < elem.durability){
+      arrDurability.push(elem.durability);
+    }
+  });
 	
-	return arrDurability.length;
+  return arrDurability.length;
 }
 
 function hasReliableWeapons(element){
-	return weapons.some(elem => element < elem.durability);
+  return weapons.some(elem => element < elem.durability);
 }
 
 function getReliableWeaponsNames(element){
-	return weapons
-		.filter(elem => element < elem.durability)
-		.map(elem => elem.name);
+  return weapons
+    .filter(elem => element < elem.durability)
+    .map(elem => elem.name);
 }
 
 function getTotalDamage(){
-	let arrAttack = 0;
+  let arrAttack = 0;
 	
-	weapons.filter(elem => arrAttack += elem.attack);
+  weapons.filter(elem => arrAttack += elem.attack);
 	
-	return arrAttack;
+  return arrAttack;
 }
 	
 function getValuestCountToSumValues(arr, sum){
-	let sumNumbers = 0;
-	let len = 0;
+  let sumNumbers = 0;
+  let len = 0;
 	
-	arr.map(function(elem){
-		if(sum > sumNumbers){
-			sumNumbers += elem;
-			len++;
-		}
-	});
+  arr.map(function(elem){
+    if(sum > sumNumbers){
+      sumNumbers += elem;
+      len++;
+    }
+  });
 	
-	return len;
+  return len;
 }
 
 //----------- 2 task -----------
@@ -77,40 +77,38 @@ function sum(...args) {
 }
 
 function compareArrays(arr1, arr2){
-	if (arr1.length !== arr2.length){
-		return false; 
-	}
+  if (arr1.length !== arr2.length){
+    return false; 
+  }
 	
-	function fun(elem, index){
-		return arr2.find((el, ind) => elem === el && index === ind);
-	}	
-	return arr1.every(fun);
+  function fun(elem, index){
+    return arr2.find((el, ind) => elem === el && index === ind);
+  }	
+  return arr1.every(fun);
 }
  
 function memorize(fn, limit){
-	const memory = [];
+  const memory = [];
 
-	function inner(...args) {
-		let results = memory.find(function(elem){
-			if (compareArrays(elem.args, args) == true) {
-				return elem;
-			}
-		});
+  function inner(...args) {
+    let results = memory.find(function(elem){
+      if (compareArrays(elem.args, args) == true) {
+        return elem;
+      }
+    });
 		
-		if(results == null) {	
-			memory.push({args: args, result: fn(...args)});
-			
-			if(memory.length > limit) { 
-				memory.shift(); 
-			}
-			
-			return memory[memory.length - 1].result;
-		} else {
-			return results.result;
-		}
-	}
+    if(results == null) {	
+      memory.push({args: args, result: fn(...args)});		
+      if(memory.length > limit) { 
+        memory.shift(); 
+      }		
+      return memory[memory.length - 1].result;
+    } else {
+      return results.result;
+    }
+  }
 	
-	return inner;
+  return inner;
 }
 
 const mSum = memorize(sum, 5);
@@ -118,16 +116,16 @@ const mSum = memorize(sum, 5);
 //----------- 3 task -----------
 
 function testFunction(array){
-	for (let i = 0; i < 100; i++) {
-		array.forEach(function(element){
-			return mSum(...element);
-		});
-	};
+  for (let i = 0; i < 100; i++) {
+    array.forEach(function(element){
+      return mSum(...element);
+    });
+  };
 }
 
 function testCase(testFunction, nameTaimer){
-	console.time(nameTaimer);
-	console.timeEnd(nameTaimer);
+  console.time(nameTaimer);
+  console.timeEnd(nameTaimer);
 }
 
 const arr = [ [1,2,3], [1,2], [1,2,3], [1,2], [9,5,2,4] ];
